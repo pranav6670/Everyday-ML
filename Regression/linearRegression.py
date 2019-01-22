d2cbd49c4aef521851d3ccf64b3801e0eb00aba4
Pranav <pranavnat24@gmail.com>
2019-01-22 20:48:45 +0530

    Simple neuron modelling logic gates

--- Regression/linearRegression.py
+++ Regression/linearRegression.py
@@ -1,4 +1,4 @@
-__author__ = "Pranav"
+__author__ = "Pranav Natekar"
 
 import numpy as np
 import pandas as pd
--- simpleMPNeuron.py
+++ simpleMPNeuron.py
@@ -1,34 +1,74 @@
+__author__ = "Pranav Natekar"
+
 import numpy as np
-import matplotlib.pyplot as plt
 
-A = np.array([0, 0, 1, 1])
-B = np.array([0, 1, 0, 1])
+# Inputs for gates
+inp = [(0, 0), (1, 0), (0, 1), (1, 1)]
+# Input for not gate
+inp_not = [0, 1]
 
-w1, w2 = 1, 1
+# Perceptron rule states that:
+#  y' = 1  if (W*x + b) >= 0
+#     = 0  if (W*x + b) <  0
+#
+# where,
+# W = weights, x = input, b = bias, y' = predicted output
 
-threshold = 0
-type = input("Enter the type of gate")
-print(type)
 
-if type == 'AND':
-    inverse = 0
-    threshold = 2
-elif type =='OR':
-    inverse = 0
-    threshold = 1
-elif type == 'NAND':
-    inverse = 1
-    threshold = 2
-elif type == 'NOT':
-    inverse = 1
-    threshold = 0
-elif type == 'NOR':
-    inverse = 1
-    threshold = 1
+def AND(x1, x2):
+    """
+    Function to model the AND gate
+    :param x1: 1st input to AND gate
+    :param x2: 2nd input to AND gate
+    :return: 0, 1 based on input
+    """
+    # x1 x2 y
+    # 0  0  0
+    # 0  1  0
+    # 1  0  0
+    # 1  1  1
+    x = np.array([x1, x2])
+    w = np.array([1, 1])
+    y = np.sum(w*x)
+    if y <= 0:
+        return 0
+    else:
+        return 1
+
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([1, 1])
+    y = np.sum(w*x)
+    if y <= 0:
+        return 0
+    else:
+        return 1
+
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-1, -1])
+    y = np.sum(w*x)
+    if y <= 0:
+        return 0
+    else:
+        return 1
+
+
+typeof = input("Enter the type of gate")
+# print(type)
+
+
+if typeof == 'AND':
+    print("AND gate selected")
+elif typeof == 'OR':
+    print("OR gate selected")
+elif typeof == 'NAND':
+    print("NAND gate selected")
+elif typeof == 'NOR':
+    print("NOR gate selected ")
+elif typeof == 'NOT':
+    print("NOT gate selected ")
 else:
     print("Don't come here")
-
-# X = np.transpose((np.transpose(A), np.transpose(B)))
-# print(X)
-sop = A * w1+ B * w2
-print(sop)
