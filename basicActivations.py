40cd65012d55e41d3deb5b3432eef829c27c5d2c
Pranav <pranavnat24@gmail.com>
2019-01-23 20:50:10 +0530

    Basic activation functions

--- basicActivations.py
+++ basicActivations.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(-5, 5, 0.1)
+x = np.arange(-10, 10, 0.1)
 
 
 def binarystep(x):
@@ -22,18 +22,19 @@ def tanh(x):
     return numerator / denominator
 
 
-
 t = tanh(x)
 b = binarystep(x)
 s = sigmoid(x)
 r = relu(x)
 
 plt.figure(1)
-plt.plot(x, r, label='ReLU', color='green', linestyle='-.')
-plt.plot(x, t, label='Tanh', color='pink', lw=1, linestyle=None)
-plt.legend()
-plt.figure(2)
-plt.plot(x, b, label='Binary Step', color='red', linestyle=None)
 plt.plot(x, s, label='Sigmoid', color='blue', linestyle='--')
+plt.plot(x, t, label='tanh', color='red', linestyle=None)
 plt.legend()
+
+plt.figure(2)
+plt.plot(x, r, label='ReLU', color='green', linestyle='-.')
+plt.plot(x, b, label='Binary Step', color='red', linestyle=None)
+plt.legend()
+
 plt.show()
