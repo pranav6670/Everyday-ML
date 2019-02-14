96d545a7c5c1c2e3e266e36184206dd4aa83fc82
Pranav <pranavnat24@gmail.com>
2019-02-14 11:26:11 +0530

    SVM

--- SVM.py
+++ SVM.py
@@ -1,12 +1,45 @@
+
+You have to choose only 2 features to do this. The reason is that you cannot plot a 7D plot. After selecting the 2 features use only these for the visualization of the decision surface.
+
+Now, the next question that you would ask if How can I choose these 2 features?. Well, there are a lot of ways. You could do a univariate F-value (feature ranking) test and see what features/variables are the most important. Then you could use these for the plot. Also, we could reduce the dimensionality from 7 to 2 using PCA for example.
+
+2D plot for 2 features and using the iris dataset
+
 from sklearn.svm import SVC
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import datasets
+from sklearn import svm, datasets
 
 iris = datasets.load_iris()
+# Select 2 features / variable for the 2D plot that we are going to create.
+X = iris.data[:, :2]  # we only take the first two features.
+y = iris.target
 
-X = iris.data
-Y = iris.target
+def make_meshgrid(x, y, h=.02):
+    x_min, x_max = x.min() - 1, x.max() + 1
+    y_min, y_max = y.min() - 1, y.max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    return xx, yy
+
+def plot_contours(ax, clf, xx, yy, **params):
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    out = ax.contourf(xx, yy, Z, **params)
+    return out
+
+model = svm.SVC(kernel='linear')
+clf = model.fit(X, y)
+
+fig, ax = plt.subplots()
+# title for the plots
+title = ('Decision surface of linear SVC ')
+# Set-up grid for plotting.
+X0, X1 = X[:, 0], X[:, 1]
+xx, yy = make_meshgrid(X0, X1)
+
+plot_contours(ax, clf, xx, yy, cmap=plt.cm.coolwarm, alpha=0.8)
+ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
+ax.set_ylabel('y label here')
 
 
 
