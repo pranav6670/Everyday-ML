f3c6a2e5a9cf05efd4a3b34c8bd414bbfe8a1dde
Pranav <pranavnat24@gmail.com>
2019-02-14 11:14:21 +0530

    Regression using neuron

---
+++ SVM.py
@@ -0,0 +1,13 @@
+from sklearn.svm import SVC
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+
+iris = datasets.load_iris()
+
+X = iris.data
+Y = iris.target
+
+
+
+
