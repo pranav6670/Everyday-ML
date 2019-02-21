3b84d5aad94aecde77873897a7a3d150ffb5b4e4
Pranav <pranavnat24@gmail.com>
2019-02-21 11:22:16 +0530

    SVM

--- multiclassSVM.py
+++ multiclassSVM.py
@@ -1,10 +1,27 @@
 from sklearn import datasets
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 
 iris = datasets.load_iris()
 
 X = iris.data
 y = iris.target
 
-x_train, x_test, y_train, y_test = train_test_split(X, y, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+# print(X_train)
+# print(X_test)
+# print(y_train)
+# print(y_test)
+
+svm_model_linear = SVC(kernel='linear', C=1).fit(X_train, y_train)
+svm_predictions = svm_model_linear.predict(X_test)
+
+# model accuracy for X_test
+accuracy = svm_model_linear.score(X_test, y_test)
+print(accuracy)
+
+# creating a confusion matrix
+cm = confusion_matrix(y_test, svm_predictions)
+print(cm)
