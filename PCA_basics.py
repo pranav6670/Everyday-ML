4f3aad33735581e2027d20b3675d2e53293c440a
Pranav <pranavnat24@gmail.com>
2019-02-28 11:15:10 +0530

    PCA

--- PCA_basics.py
+++ PCA_basics.py
@@ -1,7 +1,13 @@
 import numpy as np
 
 X = np.array([[1, 2], [3, 4], [5, 6]])
-print("Input Matrix : \n", X)
+print("Input Matrix: \n", X)
 
 mean_X = np.mean(X, axis=0)
-print("Mean : \n", mean_X)
+print("Mean: \n", mean_X)
+
+cov_mat = (X - mean_X).T.dot((X - mean_X)) / (X.shape[0]-1)
+print("Covariance matrix: \n", cov_mat)
+
+
+print("NumPy covariance matrix: \n", np.cov(X.T))
