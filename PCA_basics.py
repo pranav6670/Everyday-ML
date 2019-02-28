ba4e10c655e9ccd82119d10ee06962c658092663
Pranav <pranavnat24@gmail.com>
2019-02-28 11:50:29 +0530

    PCA

--- PCA_basics.py
+++ PCA_basics.py
@@ -1,26 +1,47 @@
 import numpy as np
 
+# Input
 X = np.array([[1, 2], [3, 4], [5, 6]])
 print("Input Matrix: \n", X)
 
-mean_X = np.mean(X, axis=0)
+# Mean of data
+mean_X = np.mean(X.T, axis=1)
 print("Mean: \n", mean_X)
 
-cov_mat = (X - mean_X).T.dot((X - mean_X)) / (X.shape[0]-1)
-print("Covariance matrix: \n", cov_mat)
+# Get zero mean data
+zero_mean = X - mean_X
+print("Zero mean data : \n", zero_mean)
 
-print("NumPy covariance matrix: \n", np.cov(X.T))
+# Get covariance
+cov_X = np.cov(zero_mean.T)
+print("Covariance matrix: \n", cov_X)
 
-cov_mat = np.cov(X.T)
-
-eig_values, eig_vectors = np.linalg.eig(cov_mat)
+# Eigen values and vectors
+eig_values, eig_vectors = np.linalg.eig(cov_X)
 
 print("Eigen vectors: \n", eig_vectors)
 print("\nEigen values \n", eig_values)
 
+# SVD
 u, s, v = np.linalg.svd(X.T)
 print("\nU:", u)
 print("\nS:", s)
 print("\nV:", v)
 
+# Check if lengths of EVs are same
+for ev in eig_vectors:
+    np.testing.assert_array_almost_equal(1.0, np.linalg.norm(ev))
+print('Everything ok!')
+
+# Make a list of (eigen value, eigen vector) tuples
+eig_pairs = [(np.abs(eig_values[i]), eig_vectors[:, i]) for i in range(len(eig_values))]
+
+# Sort the (eigen value, eigen vector) tuples from high to low
+eig_pairs.sort()
+eig_pairs.reverse()
+
+# Visually confirm that the list is correctly sorted by decreasing eigen values
+print('Eigenvalues in descending order:')
+for i in eig_pairs:
+    print(i[0])
 
