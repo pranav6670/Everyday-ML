3de5cfb90170d274c5bcb3bc879190e8eaf66034
Pranav <pranavnat24@gmail.com>
2019-02-28 11:27:35 +0530

    PCA

--- PCA_basics.py
+++ PCA_basics.py
@@ -9,5 +9,18 @@ print("Mean: \n", mean_X)
 cov_mat = (X - mean_X).T.dot((X - mean_X)) / (X.shape[0]-1)
 print("Covariance matrix: \n", cov_mat)
 
-
 print("NumPy covariance matrix: \n", np.cov(X.T))
+
+cov_mat = np.cov(X.T)
+
+eig_values, eig_vectors = np.linalg.eig(cov_mat)
+
+print("Eigen vectors: \n", eig_vectors)
+print("\nEigen values \n", eig_values)
+
+u, s, v = np.linalg.svd(X.T)
+print("\nU:", u)
+print("\nS:", s)
+print("\nV:", v)
+
+
