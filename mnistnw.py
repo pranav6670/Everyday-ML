f251f5f719a2d02374a591a0f03a2c741f0aa7d8
Pranav <pranavnat24@gmail.com>
2019-05-03 12:27:58 +0530

    NN for Mnist

---
+++ mnistnw.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+from tensorflow import keras
+
+(train_images, train_labels), (test_images, test_labels) = \
+    keras.datasets.mnist.load_data()
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(64, activation='relu'),
+    keras.layers.Dense(32, activation='relu'),
+    keras.layers.Dense(16, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print('test acc = ', test_acc)
+
+predictions = model.predict(test_images)
+print(predictions)
